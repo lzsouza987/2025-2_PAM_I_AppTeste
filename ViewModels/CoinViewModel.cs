870064445ea88a35243c66217341bea28fc56d52
lzsouza987 <luizfernando987@gmail.com>
2025-09-06 11:58:29 -0300

    Finalização da Aula. Tema MVVM

---
+++ ViewModels/CoinViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppTeste.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AppTeste.ViewModels
+{
+    public partial class CoinViewModel : ObservableObject
+    {
+        public string _ladoEscolhido = string.Empty;
+        public string _imagem = string.Empty;
+        public string _resultado = string.Empty;
+
+        public void Flip()
+        {
+            Coin coin = new Coin();
+            _resultado = coin.Jogar(_ladoEscolhido);
+        }
+
+
+
+    }
+}
