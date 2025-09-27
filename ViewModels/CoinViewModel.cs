56807a70a1366cac6a44241cb7b9384fb73864d4
lzsouza987 <luizfernando987@gmail.com>
2025-09-27 01:28:43 -0300

    Aula 02: MVVM com finalização do Jogo Cara ou Coroa. Uso de Imagens e edição do appshell.

--- ViewModels/CoinViewModel.cs
+++ ViewModels/CoinViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using AppTeste.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 
@@ -10,15 +11,35 @@ namespace AppTeste.ViewModels
 {
     public partial class CoinViewModel : ObservableObject
     {
+        public CoinViewModel()
+        {
+            FlipCommand = new Command(Flip);
+        }
+        public ICommand FlipCommand { get; private set; }
+
+        [ObservableProperty]
         public string _ladoEscolhido = string.Empty;
+
+        [ObservableProperty]
         public string _imagem = string.Empty;
+
+        [ObservableProperty]
         public string _resultado = string.Empty;
 
         public void Flip()
         {
             Coin coin = new Coin();
             _resultado = coin.Jogar(_ladoEscolhido);
+            _imagem = $"{coin.Lado}.png";
+
+            OnPropertyChanged(nameof(Resultado));
+            OnPropertyChanged(nameof(Imagem));
         }
+
+        
+
+      
+
 
 
 
--- Views/CoinView.xaml.cs
+++ Views/CoinView.xaml.cs
@@ -1,9 +1,13 @@
+using AppTeste.ViewModels;
+
 namespace AppTeste.Views;
 
 public partial class CoinView : ContentPage
 {
+
 	public CoinView()
 	{
 		InitializeComponent();
+		this.BindingContext = new CoinViewModel();
 	}
 }
